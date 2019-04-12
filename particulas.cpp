e5de33e69cabb89a90b0a3208b39252c00817e28
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2019-04-12 17:39:12 -0500

    proyecto inicial

---
+++ particulas.cpp
@@ -0,0 +1,151 @@
+#define GLUT_DISABLE_ATEXIT_HACK
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+/*
+using namespace std;
+#include <iostream>
+*/
+#define RED 0
+#define GREEN 0
+#define BLUE 0
+#define ALPHA 1
+
+#define ECHAP 27
+void init_scene();
+void render_scene();
+GLvoid initGL();
+GLvoid window_display();
+GLvoid window_reshape(GLsizei width, GLsizei height);
+GLvoid window_key(unsigned char key, int x, int y);
+
+//function called on each frame
+GLvoid window_idle();
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+
+
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Particulas");
+
+
+	initGL();
+	init_scene();
+
+	glutDisplayFunc(&window_display);
+
+	glutReshapeFunc(&window_reshape);
+
+	glutKeyboardFunc(&window_key);
+
+	//function called on each frame
+	glutIdleFunc(&window_idle);
+
+	glutMainLoop();
+
+	return 1;
+}
+
+
+
+GLvoid initGL()
+{
+	GLfloat position[] = { 0.0f, 5.0f, 10.0f, 0.0 };
+
+	//enable light : try without it
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glEnable(GL_LIGHTING);
+	//light 0 "on": try without it
+	glEnable(GL_LIGHT0);
+
+	//shading model : try GL_FLAT
+	glShadeModel(GL_SMOOTH);
+
+	glEnable(GL_DEPTH_TEST);
+
+	//enable material : try without it
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glClearColor(RED, GREEN, BLUE, ALPHA);
+}
+
+/////////////////////////////////
+//Particula
+/////////////////////////////////
+
+// Variables de rotacion
+float vs = 0.5;
+float rsol = 0;
+
+void sol(){
+	glColor3f(1.0,1.0,0.0);
+	glRotatef(rsol,0,1,0);
+	glTranslatef(0,0,0);
+	glutSolidSphere(4,8,8);
+	rsol+=vs;
+}
+
+
+GLvoid window_display()
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-25.0f, 25.0f, -25.0f, 25.0f, -25.0f, 25.0f);
+
+	sol();
+
+	glutSwapBuffers();
+
+	glFlush();
+}
+
+GLvoid window_reshape(GLsizei width, GLsizei height)
+{
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-25.0f, 25.0f, -25.0f, 25.0f, -25.0f, 25.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+
+void init_scene()
+{
+
+}
+
+GLvoid window_key(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case ECHAP:
+		exit(1);
+		break;
+
+	default:
+		printf("La touche %d non active.\n", key);
+		break;
+	}
+}
+
+
+//function called on each frame
+GLvoid window_idle()
+{
+
+
+	glutPostRedisplay();
+}
