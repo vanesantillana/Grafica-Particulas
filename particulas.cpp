3d5d6146f84edb3477ca3873ce06f9a79b2bf2a0
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2019-04-17 17:23:51 -0500

    agrege vector

--- particulas.cpp
+++ particulas.cpp
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/glut.h>
-#include <Vector>
-//#include <particula.h>
-/*
+#include <vector>
+#include "particula.h"
+
 using namespace std;
 #include <iostream>
-*/
+
 #define RED 0
 #define GREEN 0
 #define BLUE 0
@@ -20,10 +20,26 @@ void render_scene();
 float screen_width = 800;
 float screen_height = 800;
 
+#define frand()			((float)rand()/RAND_MAX)
+//#define M_PI			3.14159265
+#define MAX_PARTICULAS  10
+
+typedef struct {
+	float	life;		// vida
+	float	fade;		// fade
+	float	r, g, b;    // color
+	GLfloat x, y, z;    // posicion
+	GLfloat vx, vy, vz; // velocidad 
+    GLfloat ax, ay, az; // aceleracion
+} Particle;
+
+Particle particula[MAX_PARTICULAS];
+
 GLvoid initGL();
 GLvoid window_display();
 GLvoid window_reshape(GLsizei width, GLsizei height);
 GLvoid window_key(unsigned char key, int x, int y);
+
 
 //function called on each frame
 GLvoid window_idle();
@@ -57,6 +73,32 @@ int main(int argc, char **argv)
 }
 
 
+void iniParticulas(void)
+{
+ GLfloat v, theta, phi;
+ int i;
+
+ for(i=0; i<MAX_PARTICULAS; i++)
+    {
+	v = 0.8*frand()+0.2;
+    phi = frand()*M_PI;
+    theta = 2.0*frand()*M_PI;
+    particula[i].x = 0.0f;
+    particula[i].y = 50.0f;
+    particula[i].z = 0.0f;
+    particula[i].vx = v * cos(theta) * sin(phi);
+    particula[i].vy = v * cos(phi);
+    particula[i].vz = v * sin(theta) * sin(phi);
+	particula[i].ax =  0.01f;
+    particula[i].ay = -0.08f;
+    particula[i].az =  0.0f;
+	particula[i].r = 0.882f;
+	particula[i].g = 0.552f;	
+	particula[i].b = 0.211f;	
+	particula[i].life = 1.0f;		                
+	particula[i].fade = 0.01f;	
+	}
+}
 
 GLvoid initGL()
 {
@@ -95,20 +137,22 @@ void sol(){
 	glutSolidSphere(4,8,8);
 	rsol+=vs;
 }
-
 /////////////////////////////////
 //Particula
 /////////////////////////////////
-void dibujar(float x, float y){
-	//Particula (x,y);
+void crear_particula (){
+	Particula p(0,0);
+	cout<<"Cree una particula"<<endl;
 }
-Vector mouseCoord;
+
+/*Vector mouseCoord;
 
 void Mouse(int btn,int state,int x,int y) {
 	mouseCoord.x = x - screen_width/2;
 	mouseCoord.y = -y + screen_height/2;
 	printf(mouseCoord.x);
 }
+*/
 
 GLvoid window_display()
 {
@@ -120,8 +164,10 @@ GLvoid window_display()
 	glLoadIdentity();
 	glOrtho(-25.0f, 25.0f, -25.0f, 25.0f, -25.0f, 25.0f);
 
-	//sol();
-	glutMouseFunc(Mouse);
+	iniParticulas();
+
+	//crear_particula();
+	//glutMouseFunc(Mouse);
 
 	glutSwapBuffers();
 
@@ -159,6 +205,37 @@ GLvoid window_key(unsigned char key, int x, int y)
 	}
 }
 
+void display(void)
+{
+	//cout<<"ingrese al display"<<endl;
+	int i;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt (0.0, 0.0, 150.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+/*
+	glEnable(GL_TEXTURE_2D);
+	MyTextura.SetActive();
+*/
+	for (i=0; i<MAX_PARTICULAS; i++)
+	{
+	glColor4f(particula[i].r,particula[i].g,particula[i].b, particula[i].life);
+	glBegin(GL_TRIANGLE_STRIP);				        
+		glTexCoord2d(1,1); glVertex3f(particula[i].x+1.0f,particula[i].y+1.0f,particula[i].z);            
+		glTexCoord2d(0,1); glVertex3f(particula[i].x-1.0f,particula[i].y+1.0f,particula[i].z);       
+		glTexCoord2d(1,0); glVertex3f(particula[i].x+1.0f,particula[i].y-1.0f,particula[i].z);        
+		glTexCoord2d(0,0); glVertex3f(particula[i].x-1.0f,particula[i].y-1.0f,particula[i].z);      
+	glEnd();	
+	particula[i].x += particula[i].vx;
+	particula[i].y += particula[i].vy;
+	particula[i].z += particula[i].vz;
+	particula[i].vx += particula[i].ax;
+	particula[i].vy += particula[i].ay;
+	particula[i].vz += particula[i].az;
+	particula[i].life -= particula[i].fade;	
+	}
+	glutSwapBuffers();
+}
 
 //function called on each frame
 GLvoid window_idle()
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Particulas
+Proyecto teorico de Grafica Computacional
+
+### Compilar
+El siguiente comando para compilar
+```
+gcc particulas.cpp `pkg-config opencv --libs --cflags` -o foo -lstdc++ -lglut -lGLU -lGL -lm
+```
+
+### Tareas
+* Obtener posicion del Puntero
+* Crear particula con su clase
+* Generar varias particulas a partir del click del puntero
